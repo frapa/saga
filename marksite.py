0c51f75018af8ce7d9bc264447a75824480e8dfb
Francesco Pasa <francescopasa@gmail.com>
2012-01-16 20:24:11 +0100

    Added listing of directory and update of tree

--- marksite.py
+++ marksite.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
 	parser.add_argument("path")
 	# command line actions
 	parser.add_argument("-i", "--init", action="store_true", help="Init a site project")
+	parser.add_argument("-l", "--list", action="store_true", help="Lists the website tree structure")
 	parser.add_argument("-u", "--update", action="store_true", help="Check for changes")
 	parser.add_argument("-r", "--reset", action="store_true", help="Resets a project removing all metadata")
 	parser.add_argument("-b", "--build", action="store_true", help="Builds markdown files to html, applying the theme")
@@ -23,14 +24,32 @@ if __name__ == "__main__":
 	
 	# useful paths
 	base_path = os.path.abspath(args.path) # absolute path to the directory that contains the website
-	src_path = os.path.join(base_path, "source")
-	build_path = os.path.join(base_path, "build")
+	src_path = os.path.join(base_path, "source/")
+	build_path = os.path.join(base_path, "build/")
 	
 	if args.init:
 		# build the file tree
 		site_tree = tree.tree(src_path)
 	
-	if args.init:
+	if args.list or args.update:
+		# load saved tree
+		try:
+			with open(os.path.join(base_path, "tree.obj"), "rb") as infile:
+				site_tree = pickle.load(infile)
+		except IOError:
+			# this means file does not exists
+			print("This project as not been inizialized.")
+			exit(1)
+	
+	if args.list:
+		# print tree structure
+		print(site_tree)
+	
+	if args.update:
+		# update file tree
+		site_tree = tree.update(site_tree)
+	
+	if args.init or args.update:
 		# save build file tree
 		with open(os.path.join(base_path, "tree.obj"), "wb") as outfile:
 			pickle.dump(site_tree, outfile)
--- tree.py
+++ tree.py
@@ -4,7 +4,9 @@ import hashlib
 # file and directory status costants
 NEW = 0
 CHANGED = 1
-TO_BE_REMOVED = 2
+BUILT = 2
+UP_TO_DATE = 3
+DELETED = 4
 
 
 # efficentley calculate md5 of a file
@@ -26,6 +28,7 @@ def md5_file(path):
 class Node:
 	def __init__(self, path, parent):
 		self.path = path
+		self.name = os.path.basename(path)
 		self.parent = parent
 	
 	def get_parent(self):
@@ -57,8 +60,21 @@ class File(Node):
 			
 			# calculate md5 for the file
 			self.md5 = md5_file(abs_path)
+			self.status = NEW
 		else:
-			raise IOError("Argument must be path to a valid file.")
+			raise IOError("Argument must be path to a valid file. Got '{0}'.".format(abs_path))
+	
+	# checks if file has same md5 as before
+	# set status = CHANGED if it has
+	def update(self):
+		abs_path = os.path.join(self.parent.get_root().path, self.path)
+		
+		# calculate new md5 of the file
+		new_md5 = md5_file(abs_path)
+		
+		if new_md5 != self.md5:
+			self.md5 = new_md5
+			self.status = CHANGED
 
 
 # subclass of Node representing a directory
@@ -67,6 +83,7 @@ class Folder(Node):
 		super(Folder, self).__init__(path, parent)
 		
 		self.children = {}
+		self.status = NEW
 	
 	def add_child(self, name, node):
 		self.children[name] = node
@@ -84,6 +101,23 @@ class Folder(Node):
 			return self.children[path[0]]
 		else:
 			return self.children[path[0]].get_path(path[1:])
+	
+	# return string representation of the object
+	def __str__(self):
+		s = self.name
+		cd = self # current directory
+		level = 1 # indentation level
+		
+		for name, child in cd.children.items():
+			# newline and identation
+			s += "\n" + " |   " * (level - 1) + " +-- "
+			
+			if isinstance(child, Folder):
+				s += str(child).replace("\n", "\n{0}".format(" |   " * level))
+			else:
+				s += name
+		
+		return s
 
 
 # functions which builds and return a tree object
@@ -91,6 +125,10 @@ class Folder(Node):
 def tree(path):
 	# this is the tree which starts with the root folder
 	t = Folder(path, None)
+	
+	# assign name to project
+	if path[-1] == os.sep:
+		t.name = os.path.basename(os.path.dirname(path[:-1]))
 	
 	# walk directory tree and build the tree object
 	for root, dirs, files in os.walk(path):
@@ -106,5 +144,33 @@ def tree(path):
 		for f in files:
 			rpath = os.path.join(rel_path, f) # relative path of file
 			cd.add_child(f, File(rpath, cd))
+	
+	return t
+
+# functions which update passed tree, adding new files, marking
+# old files if they changed and marking deleted files as no more existent
+def update(t):
+	# walk directories to update the tree object
+	for root, dirs, files in os.walk(t.path):
+		# path relative from source directory to current directory
+		rel_path = root.replace(t.path, "")
+		# current directory node object
+		cd = t.get_path(rel_path.split(os.sep))
+		
+		for d in dirs:
+			if d not in cd.children:
+				rpath = os.path.join(rel_path, d) # relative path of folder
+				cd.add_child(d, Folder(rpath, cd))
+			
+		for f in files:
+			if f in cd.children:
+				cd.children[f].update()
+			else:
+				rpath = os.path.join(rel_path, f) # relative path of file
+				cd.add_child(f, File(rpath, cd))
+		
+		for child in cd.children:
+			if child not in dirs + files:
+				child.status = DELETED
 	
 	return t
